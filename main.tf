bb3b6574a7f5d79dda23baf3750adca1856bd60c
TK <sabin.tk@hpe.com>
2022-12-11 19:03:56 +0530

    first commit

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "Test_TF" {
-  ami           = "ami-0574da719dca65348" # us-west-2
+resource "aws_instance" "Test_TF1" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
