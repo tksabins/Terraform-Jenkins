94a3fda26b10d52d38b4313210ca83cb78a8a268
TK <sabin.tk@hpe.com>
2022-12-11 19:19:05 +0530

    changes

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "Test_TF1" {
+resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
