286dfd99e85a6ca5255b7c2d3d708e6479196ac1
TK <sabin.tk@hpe.com>
2022-12-12 16:31:21 +0530

    updated

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-0574da719dca65348" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "TF-Instance1"
   }
 }
