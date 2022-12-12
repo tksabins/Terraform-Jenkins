e131efa272b7a491302889c60e18521605e3a44b
TK <sabin.tk@hpe.com>
2022-12-12 12:05:59 +0530

    newupdate

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "Test" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
